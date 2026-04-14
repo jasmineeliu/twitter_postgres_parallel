5e0f37b0206f670568029ca452dd33021e40dbe5
Jasmine Liu <jasmineliu331@gmail.com>
2026-04-13 19:00:41 -0700

    getting sequential loads working

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -7,7 +7,7 @@ echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
     echo
-    # copy your solution to the twitter_postgres assignment here
+    zcat "$file" | sed 's/\\u0000//g' | psql "postgresql://postgres:pass@localhost:1089/postgres" -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
@@ -15,7 +15,7 @@ echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
     echo
-    # copy your solution to the twitter_postgres assignment here
+    python3 load_tweets.py --inputs="$file" --db postgresql://postgres:pass@localhost:2089/postgres
 done
 
 echo '================================================================================'
